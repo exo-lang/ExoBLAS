dc7121432fbbe572ac2f5689dc9c1abd42c0cdfd
Kevin Qian <keqian@mit.edu>
2023-02-04 16:51:32 -0500

    scheduled gemv_transpose with neon

--- level2/gemv.py
+++ level2/gemv.py
@@ -31,15 +31,25 @@ def gemv(
     y[i] = beta * y[i]
     for j in seq(0, n):
       y[i] += alpha * x[j] * a[i, j]
+      # NOTE: is there a way to do additive/multiplicative associativity? for now I'm just changing the original proc...
+      # e.g if i want to bind_expr (b*c) in a*b*c = (a*b)*c, then (b*c) is not found
 
 
-neon_gemv = gemv
+@proc
+def gemv_transpose(
+  alpha: f32,
+  beta: f32,
+  n: size,
+  m: size,
+  a: f32[n, m],
+  x: f32[n],
+  y: f32[m],
+):
+  for i in seq(0, m):
+    y[i] = beta * y[i]
+    for j in seq(0, n):
+      y[i] += alpha * x[j] * a[j, i]
 
-# NOTE: is there a way to do additive/multiplicative associativity? for now I'm just changing the original proc...
-# e.g if i want to bind_expr (b*c) in a*b*c = (a*b)*c, then (b*c) is not found
-
-neon_gemv = divide_loop(neon_gemv, "i", 4, ["io", "ii"], tail="cut_and_guard")
-neon_gemv = fission(neon_gemv, neon_gemv.find("y[_] = _").after(), n_lifts=2)
 
 # NOTE: this pattern of bind_expr, expand_dim, lift_alloc, fission is pretty common,
 # but can't use stage_mem. I think something like Samir's stage_expr might be useful as
@@ -55,76 +65,128 @@ def neon_stage_expr(proc, loop_var, expr, new_name, n_lifts=1):
   # remove_loop was kinda slow depending on the order I ran it in
   return proc
 
-print("vectorizing y[_] = beta * y[_]")
+def neon_broadcast_constant(proc, const, loop_var, n_lifts=1):
+  const_vec = f"{const}_vec"
+  print(f"\tbroadcasting {const} to {const_vec}")
+  proc = neon_stage_expr(proc, loop_var, const, const_vec, n_lifts=n_lifts)
+  # NOTE: for neon, we need to convert const from f32 to f32[1]
+  const_temp = f"{const}_temp"
+  proc = stage_mem(proc, f"for {loop_var} in _:_", const, const_temp)
+  proc = expand_dim(proc, const_temp, 1, "0")
+  proc = replace(proc, f"for {loop_var} in _:_", neon_broadcast_4xf32)
+  return proc
 
-print("\tbroadcasting beta to beta_vec")
-neon_gemv = neon_stage_expr(neon_gemv, "ii", "beta", "beta_vec", n_lifts=2)
-# neon_gemv = remove_loop(neon_gemv, "io #0")
-# NOTE: needed to convert beta from f32 to f32[1]
-neon_gemv = stage_mem(neon_gemv, "for ii in _:_", "beta", "beta_temp")
-neon_gemv = expand_dim(neon_gemv, "beta_temp", 1, "0")
-neon_gemv = replace(neon_gemv, "for ii in _:_", neon_broadcast_4xf32)
+def vectorize_beta_times_y(proc):
+  print("vectorizing y[_] = beta * y[_]")
 
-print("\tmaking y[_] a vector")
-neon_gemv = neon_stage_expr(neon_gemv, "ii", "y[_]", "old_y_vec")
-neon_gemv = replace(neon_gemv, "for ii in _:_", neon_vld_4xf32)
-# NOTE: need two registers for y because no duplicate arguments allowed
-neon_gemv = simplify(stage_mem(neon_gemv, "for ii in _:_", "y[4*io:4*io+4]", "new_y_vec"))
-neon_gemv = set_memory(neon_gemv, "new_y_vec", Neon4f)
-neon_gemv = replace(neon_gemv, "for ii in _:_", neon_vmul_4xf32)
-neon_gemv = replace(neon_gemv, "for i0 in _:_", neon_vst_4xf32)
+  proc = neon_broadcast_constant(proc, "beta", "ii", n_lifts=2)
 
-print("splitting j loop...")
-neon_gemv = divide_loop(neon_gemv, "j", 4, ["jo", "ji"], tail="cut_and_guard")
-neon_gemv = fission(neon_gemv, neon_gemv.find("for jo in _:_").after())
-neon_gemv = reorder_loops(neon_gemv, "ii jo")
+  print("\tmaking y[_] a vector")
+  proc = neon_stage_expr(proc, "ii", "y[_]", "old_y_vec")
+  proc = replace(proc, "for ii in _:_", neon_vld_4xf32)
+  # NOTE: need two registers for y because no duplicate arguments allowed
+  proc = simplify(stage_mem(proc, "for ii in _:_", "y[4*io:4*io+4]", "new_y_vec"))
+  proc = set_memory(proc, "new_y_vec", Neon4f)
+  proc = replace(proc, "for ii in _:_", neon_vmul_4xf32)
+  proc = replace(proc, "for i0 in _:_", neon_vst_4xf32)
 
-print("vectorizing y[_] += alpha * x[_] * a[_]")
+  return proc
 
-print("\tbroadcasting alpha to alpha_vec")
-neon_gemv = neon_stage_expr(neon_gemv, "ji", "alpha", "alpha_vec", n_lifts=4)
-# NOTE: need to convert alpha from f32 to f32[1]
-neon_gemv = stage_mem(neon_gemv, "for ji in _:_", "alpha", "alpha_temp")
-neon_gemv = expand_dim(neon_gemv, "alpha_temp", 1, "0")
-neon_gemv = replace(neon_gemv, "for ji in _:_", neon_broadcast_4xf32)
+def schedule_gemv_transpose_on_neon():
+  proc = gemv_transpose
+  proc = divide_loop(proc, "i", 4, ["io", "ii"], tail="cut_and_guard")
+  proc = fission(proc, proc.find("y[_] = _").after(), n_lifts=2)
 
-print("\tmaking y a vector")
-neon_gemv = simplify(stage_mem(neon_gemv, "for jo in _:_", "y[4*io:4*io+4]", "y_vec"))
-neon_gemv = set_memory(neon_gemv, "y_vec", Neon4f)
-neon_gemv = replace(neon_gemv, "for i0 in _:_", neon_vld_4xf32)
-neon_gemv = replace(neon_gemv, "for i0 in _:_", neon_vst_4xf32)
+  proc = vectorize_beta_times_y(proc)
 
-print("\tvectorizing alpha[_] * x[_]")
-neon_gemv = neon_stage_expr(neon_gemv, "ji", "alpha_vec[_] * x[_]", "alpha_times_x", n_lifts=2)
-neon_gemv = simplify(stage_mem(neon_gemv, "for ji in _:_", "x[4*jo:4*jo+4]", "x_vec"))
-neon_gemv = set_memory(neon_gemv, "x_vec", Neon4f)
-neon_gemv = replace(neon_gemv, "for i0 in _:_", neon_vld_4xf32)
-neon_gemv = replace(neon_gemv, "for ji in _:_", neon_vmul_4xf32)
+  print("splitting j loop...")
+  proc = divide_loop(proc, "j", 4, ["jo", "ji"], tail="cut_and_guard")
+  proc = fission(proc, proc.find("for jo in _:_").after())
+  proc = reorder_loops(proc, "ii jo")
 
-print("\tvectorizing alpha_times_x[_] * a[_]")
-neon_gemv = simplify(stage_mem(neon_gemv, "for ii in _:_", "a[4*io:4*io+4, 4*jo:4*jo+4]", "a_vecs"))
-neon_gemv = set_memory(neon_gemv, "a_vecs", Neon4f)
-neon_gemv = simplify(stage_mem(neon_gemv, "for i0 in _:_", "a[4*io:4*io+4, 4*jo:4*jo+4]", "a_transposed"))
-neon_gemv = rearrange_dim(neon_gemv, "a_transposed", [1, 0])
-neon_gemv = rearrange_dim(neon_gemv, "a_vecs", [1, 0])
-# TODO: how to do i0 i1 #1? it doesn't seem to work
-neon_gemv = reorder_loops(neon_gemv, neon_gemv.find("for i0 in _:_ #1"))
-neon_gemv = replace(neon_gemv, "for i0 in _:_ #1", neon_vld_4xf32)
+  print("vectorizing y[_] += alpha * x[_] * a[_]")
 
-neon_gemv = reorder_loops(neon_gemv, "ii ji")
-neon_gemv = commute_expr(neon_gemv, "alpha_times_x[_] * a_vecs[_]")
-neon_gemv = replace(neon_gemv, "for ii in _:_", neon_vfmla_4xf32_4xf32)
+  proc = neon_broadcast_constant(proc, "alpha", "ji", n_lifts=4)
 
-neon_gemv = simplify(neon_gemv)
+  print("\tmaking y a vector")
+  proc = simplify(stage_mem(proc, "for jo in _:_", "y[4*io:4*io+4]", "y_vec"))
+  proc = set_memory(proc, "y_vec", Neon4f)
+  proc = replace(proc, "for i0 in _:_", neon_vld_4xf32)
+  proc = replace(proc, "for i0 in _:_", neon_vst_4xf32)
+
+  print("\tvectorizing alpha[_] * x[_]")
+  proc = neon_stage_expr(proc, "ji", "alpha_vec[_] * x[_]", "alpha_times_x", n_lifts=2)
+  proc = simplify(stage_mem(proc, "for ji in _:_", "x[4*jo:4*jo+4]", "x_vec"))
+  proc = set_memory(proc, "x_vec", Neon4f)
+  proc = replace(proc, "for i0 in _:_", neon_vld_4xf32)
+  proc = replace(proc, "for ji in _:_", neon_vmul_4xf32)
+
+  print("\tvectorizing alpha_times_x[_] * a[_]")
+  # NOTE: swapped some indices, removed the rearrange_dims
+  proc = simplify(stage_mem(proc, "for ii in _:_", "a[4*jo:4*jo+4, 4*io:4*io+4]", "a_vecs"))
+  proc = set_memory(proc, "a_vecs", Neon4f)
+  proc = replace(proc, "for i1 in _:_", neon_vld_4xf32)
+  proc = reorder_loops(proc, "ii ji")
+  proc = commute_expr(proc, "alpha_times_x[_] * a_vecs[_]")
+  proc = replace(proc, "for ii in _:_", neon_vfmla_4xf32_4xf32)
+
+  return proc
+
+def schedule_gemv_on_neon():
+  proc = gemv
+  proc = divide_loop(proc, "i", 4, ["io", "ii"], tail="cut_and_guard")
+  proc = fission(proc, proc.find("y[_] = _").after(), n_lifts=2)
+
+  proc = vectorize_beta_times_y(proc)
+
+  print("splitting j loop...")
+  proc = divide_loop(proc, "j", 4, ["jo", "ji"], tail="cut_and_guard")
+  proc = fission(proc, proc.find("for jo in _:_").after())
+  proc = reorder_loops(proc, "ii jo")
+
+  print("vectorizing y[_] += alpha * x[_] * a[_]")
+
+  proc = neon_broadcast_constant(proc, "alpha", "ji", n_lifts=4)
+
+  print("\tmaking y a vector")
+  proc = simplify(stage_mem(proc, "for jo in _:_", "y[4*io:4*io+4]", "y_vec"))
+  proc = set_memory(proc, "y_vec", Neon4f)
+  proc = replace(proc, "for i0 in _:_", neon_vld_4xf32)
+  proc = replace(proc, "for i0 in _:_", neon_vst_4xf32)
+
+  print("\tvectorizing alpha[_] * x[_]")
+  proc = neon_stage_expr(proc, "ji", "alpha_vec[_] * x[_]", "alpha_times_x", n_lifts=2)
+  proc = simplify(stage_mem(proc, "for ji in _:_", "x[4*jo:4*jo+4]", "x_vec"))
+  proc = set_memory(proc, "x_vec", Neon4f)
+  proc = replace(proc, "for i0 in _:_", neon_vld_4xf32)
+  proc = replace(proc, "for ji in _:_", neon_vmul_4xf32)
+
+  print("\tvectorizing alpha_times_x[_] * a[_]")
+  proc = simplify(stage_mem(proc, "for ii in _:_", "a[4*io:4*io+4, 4*jo:4*jo+4]", "a_vecs"))
+  proc = set_memory(proc, "a_vecs", Neon4f)
+  proc = simplify(stage_mem(proc, "for i0 in _:_", "a[4*io:4*io+4, 4*jo:4*jo+4]", "a_transposed"))
+  proc = rearrange_dim(proc, "a_transposed", [1, 0])
+  proc = rearrange_dim(proc, "a_vecs", [1, 0])
+  # TODO: how to do i0 i1 #1? it doesn't seem to work
+  proc = reorder_loops(proc, proc.find("for i0 in _:_ #1"))
+  proc = replace(proc, "for i0 in _:_ #1", neon_vld_4xf32)
+  proc = reorder_loops(proc, "ii ji")
+  proc = commute_expr(proc, "alpha_times_x[_] * a_vecs[_]")
+  proc = replace(proc, "for ii in _:_", neon_vfmla_4xf32_4xf32)
+
+  proc = simplify(proc)
+  return proc
 
 if __name__ == "__main__":
+  # final_proc = schedule_gemv_on_neon()
+  final_proc = schedule_gemv_transpose_on_neon()
+
   print("="*50)
   print("Original proc:")
   print(gemv)
   print("="*50)
   print("Final scheduled proc:")
-  print(neon_gemv)
-  compile_procs([neon_gemv], Path(os.path.expanduser("~/Documents/BLAS/temp")), "neon_gemv.c", "neon_gemv.h")
-  print("Compiled to C!")
+  print(final_proc)
 
-__all__ = ["gemv", "neon_gemv"]
+  compile_procs([final_proc], Path(os.path.expanduser("~/Documents/BLAS/temp")), "proc.c", "proc.h")
+  print("Compiled to C!")
