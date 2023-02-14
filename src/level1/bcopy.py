ee07a29b83a50958d9f3691c35d7a99f6a1e4332
Alex Reinking <alex.reinking@gmail.com>
2023-02-14 15:21:43 -0500

    use machine params for bcopy

--- src/level1/bcopy.py
+++ src/level1/bcopy.py
@@ -6,33 +6,49 @@ from exo.platforms.x86 import *
 from exo.syntax import *
 from exo.stdlib.scheduling import *
 
+import exo_blas_config as C
+
+
 @proc
 def copy_template(n: size, x: f32[n], y: f32[n]):
     for i in seq(0, n):
         y[i] = x[i]
-        
-def schedule_axpy_stride_1(VEC_W, memory, instructions):
-    simple_stride_1 = rename(copy_template, copy_template.name() + "_simple_stride_1")
-    simple_stride_1 = simple_stride_1.add_assertion("stride(x, 0) == 1")
-    simple_stride_1 = simple_stride_1.add_assertion("stride(y, 0) == 1")
-    
-    loop_fragment = lambda iter, idx=0: f"for {iter} in _:_ #{idx}"
 
-    simple_stride_1 = divide_loop(simple_stride_1, loop_fragment("i"), VEC_W, ("io", "ii"), tail="cut")
-    simple_stride_1 = stage_mem(simple_stride_1, loop_fragment("ii"), f"x[io * {VEC_W}:(io + 1) * {VEC_W}]", "xReg")
-    
-    simple_stride_1 = set_memory(simple_stride_1, "xReg", memory)
-    simple_stride_1 = replace_all(simple_stride_1, instructions)
 
-    return simplify(simple_stride_1)
+simple_stride_1 = rename(copy_template, copy_template.name() + "_simple_stride_1")
+simple_stride_1 = simple_stride_1.add_assertion("stride(x, 0) == 1")
+simple_stride_1 = simple_stride_1.add_assertion("stride(y, 0) == 1")
 
-copy_stride_1 = schedule_axpy_stride_1(8, AVX2, [mm256_loadu_ps, mm256_storeu_ps])
+
+def loop_fragment(it, idx=0):
+    return f"for {it} in _: _ #{idx}"
+
+
+simple_stride_1 = divide_loop(
+    simple_stride_1, loop_fragment("i"), C.Machine.vec_width, ("io", "ii"), tail="cut"
+)
+simple_stride_1 = stage_mem(
+    simple_stride_1,
+    loop_fragment("ii"),
+    f"x[io * {C.Machine.vec_width}:(io + 1) * {C.Machine.vec_width}]",
+    "xReg",
+)
+
+simple_stride_1 = set_memory(simple_stride_1, "xReg", C.Machine.mem_type)
+simple_stride_1 = replace_all(
+    simple_stride_1, [C.Machine.load_instr, C.Machine.store_instr]
+)
+
+copy_stride_1 = simplify(simple_stride_1)
+
 
 @proc
-def bcopy(n: size, x: f32[n], y: f32[n]):
+def exo_bcopy(n: size, x: f32[n], y: f32[n]):
     assert stride(x, 0) == 1
     assert stride(y, 0) == 1
     copy_stride_1(n, x, y)
+
+
 """
 TODO: Should be:
 if stride(x, 0) == 1 and stride(y, 0) == 1:
@@ -42,10 +58,8 @@ else:
     copy_template(n, x, y)
 """
 
-bcopy = rename(bcopy, "exo_bcopy")
-
 if __name__ == "__main__":
     print(copy_stride_1)
-    print(bcopy)
+    print(exo_bcopy)
 
-__all__ = ["bcopy"]
+__all__ = ["exo_bcopy"]
