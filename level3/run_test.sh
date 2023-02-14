dfa76271dd232d2513e6a9f3581ff650eba6833e
Hooninator <jbellavita@berkeley.edu>
2023-02-14 00:05:15 +0000

    add more compiler flags

--- level3/run_test.sh
+++ level3/run_test.sh
@@ -7,6 +7,6 @@ else
     python3 $1.py --kc $2 --mc $3 --mr $4 --nr $5
     cd c/$1
     export OPENBLAS_NUM_THREADS=1
-    g++ test_$1.cpp -lopenblas -o $1 -O3 -fpermissive
+    g++ test_$1.cpp -lopenblas -o $1 -O3 -fpermissive -ffast-math -ffp-contract=fast
     ./$1 $6
 fi
