e3814c04c4b4c22a7d1756873006709d0583f5f3
Samir Droubi <samirdr@mit.edu>
2023-02-22 14:36:37 -0500

    interleaved axpy schedule

--- src/level1/axpy.py
+++ src/level1/axpy.py
@@ -58,6 +58,69 @@ def schedule_axpy_stride_1(VEC_W, memory, instructions):
     
     return simplify(simple_stride_1)
 
+def schedule_interleave_axpy_stride_1(VEC_W, INTERLEAVE_FACTOR, memory, instructions):
+    simple_stride_1 = rename(axpy_template, axpy_template.name() + "_simple_stride_1")
+    simple_stride_1 = simple_stride_1.add_assertion("stride(x, 0) == 1")
+    simple_stride_1 = simple_stride_1.add_assertion("stride(y, 0) == 1")
+
+    def stage_expr(proc, expr, buffer):
+        proc = bind_expr(proc, expr, buffer)
+        proc = expand_dim(proc, buffer, VEC_W, "ii")
+        proc = lift_alloc(proc, f"{buffer}:_", n_lifts=1)
+        proc = fission(proc, proc.find(f"{buffer}[_] = _").after())
+        return proc
+    
+    def hoist_const_loop(proc, constant):
+        while True:
+            try:
+                loop_cursor = proc.find(constant).parent().parent()
+                proc = reorder_stmts(proc, loop_cursor.expand(-1))
+            except:
+                break
+        loop_cursor = proc.find(constant).parent().parent()
+        proc = autofission(proc, loop_cursor.after())
+        return proc
+
+    loop_fragment = lambda iter, idx=0: f"for {iter} in _:_ #{idx}"
+    simple_stride_1 = divide_loop(simple_stride_1, loop_fragment("i"), VEC_W * INTERLEAVE_FACTOR, ("io", "ii"), tail="cut")
+    simple_stride_1 = divide_loop(simple_stride_1, loop_fragment("ii"), VEC_W, ("im", "ii"), perfect=True)
+        
+    simple_stride_1 = stage_expr(simple_stride_1, "alpha", "alphaReg")
+    simple_stride_1 = expand_dim(simple_stride_1, "alphaReg", INTERLEAVE_FACTOR, "im")
+    simple_stride_1 = lift_alloc(simple_stride_1, "alphaReg: _", 2)
+    simple_stride_1 = hoist_const_loop(simple_stride_1, "alpha")
+    simple_stride_1 = autofission(simple_stride_1, simple_stride_1.find(loop_fragment("im")).after())
+        
+    simple_stride_1 = stage_expr(simple_stride_1, "x[_]", "xReg")
+    y_lower_bound = f"{VEC_W * INTERLEAVE_FACTOR} * io + {VEC_W} * im"
+    simple_stride_1 = simplify(stage_mem(simple_stride_1, loop_fragment("ii", 2), 
+                                         f"y[{y_lower_bound}:{y_lower_bound} + {VEC_W}]", "yReg"))
+    
+    simple_stride_1 = expand_dim(simple_stride_1, "xReg", INTERLEAVE_FACTOR, "im")
+    simple_stride_1 = lift_alloc(simple_stride_1, "xReg : _")
+    simple_stride_1 = expand_dim(simple_stride_1, "yReg", INTERLEAVE_FACTOR, "im")
+    simple_stride_1 = lift_alloc(simple_stride_1, "yReg : _")
+    
+    for buffer in ["xReg", "alphaReg", "yReg"]:
+        simple_stride_1 = set_memory(simple_stride_1, buffer, memory)
+        simple_stride_1 = set_precision(simple_stride_1, buffer, "f32")
+
+    simple_stride_1 = replace_all(simple_stride_1, instructions)
+    
+    def interleave_instructions(proc, iter):
+        while True:
+            main_loop = proc.find(f"for {iter} in _:_")
+            if len(main_loop.body()) == 1:
+                break
+            proc = fission(proc, main_loop.body()[0].after())
+            proc = unroll_loop(proc, f"for {iter} in _:_")
+        proc = unroll_loop(proc, "for im in _:_")
+        return proc
+    
+    simple_stride_1 = interleave_instructions(simple_stride_1, "im")
+    simple_stride_1 = interleave_instructions(simple_stride_1, "im")
+    return simplify(simple_stride_1)
+
 instructions = [C.Machine.load_instr_f32,
                      C.Machine.store_instr_f32,
                      C.Machine.broadcast_scalar_instr_f32,
@@ -65,7 +128,7 @@ instructions = [C.Machine.load_instr_f32,
                      ]
 
 if None not in  instructions:
-    axpy_stride_1 = schedule_axpy_stride_1(C.Machine.vec_width, C.Machine.mem_type, instructions)
+    axpy_stride_1 = schedule_interleave_axpy_stride_1(C.Machine.vec_width, 4, C.Machine.mem_type, instructions)
 else:
     axpy_stride_1 = axpy_template
 
