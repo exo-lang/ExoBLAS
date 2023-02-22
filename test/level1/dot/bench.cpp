1d1872ab91ac445a318974aa40ac60a1b29db6f9
Samir Droubi <samirdr@mit.edu>
2023-02-22 16:25:25 -0500

    dot testing

--- test/level1/dot/bench.cpp
+++ test/level1/dot/bench.cpp
@@ -7,28 +7,6 @@
 
 #include "exo_dot.h"
 
-static float naive_sdot(const int N, const float  *X, const int incX,
-                  const float  *Y, const int incY) {
-    float result = 0.0;
-    for (int i = 0; i < N; ++i) {
-        result += X[i * incX] + Y[i * incY];
-    }
-    return result;
-}
-
-static void BM_NAIVE_SDOT(benchmark::State& state) {
-    auto n = state.range(0);
-
-    std::vector<float> x = generate1d_sbuffer(n, 1);
-    std::vector<float> y = generate1d_sbuffer(n, 1);
-
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(naive_sdot(n, x.data(), 1, y.data(), 1));
-    }
-
-    // state.counters["flops"] = ;
-}
-
 static void BM_CBLAS_SDOT(benchmark::State& state) {
     auto n = state.range(0);
 
@@ -50,33 +28,117 @@ static void BM_EXO_SDOT(benchmark::State& state) {
     float result = 0.0;
 
     for (auto _ : state) {
-        exo_sdot(nullptr, n, exo_win_1f32c{x.data(), 1}, exo_win_1f32c{y.data(), 1}, &result);
+        exo_sdot(nullptr, n, exo_win_1f32c{x.data(), {1}}, exo_win_1f32c{y.data(), {1}}, &result);
     }
 
     // state.counters["flops"] = ;
 }
 
 // Register the function as a benchmark
+BENCHMARK(BM_CBLAS_SDOT) -> Args({1});
+BENCHMARK(BM_EXO_SDOT) -> Args({1});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({2});
+BENCHMARK(BM_EXO_SDOT) -> Args({2});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({4});
+BENCHMARK(BM_EXO_SDOT) -> Args({4});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({8});
+BENCHMARK(BM_EXO_SDOT) -> Args({8});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({16});
+BENCHMARK(BM_EXO_SDOT) -> Args({16});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({32});
+BENCHMARK(BM_EXO_SDOT) -> Args({32});
+
+
 BENCHMARK(BM_CBLAS_SDOT) -> Args({64});
-BENCHMARK(BM_NAIVE_SDOT) -> Args({64});
 BENCHMARK(BM_EXO_SDOT) -> Args({64});
 
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({128});
+BENCHMARK(BM_EXO_SDOT) -> Args({128});
+
+
 BENCHMARK(BM_CBLAS_SDOT) -> Args({256});
-BENCHMARK(BM_NAIVE_SDOT) -> Args({256});
 BENCHMARK(BM_EXO_SDOT) -> Args({256});
 
+
 BENCHMARK(BM_CBLAS_SDOT) -> Args({512});
-BENCHMARK(BM_NAIVE_SDOT) -> Args({512});
 BENCHMARK(BM_EXO_SDOT) -> Args({512});
 
-BENCHMARK(BM_CBLAS_SDOT) -> Args({100000});
-BENCHMARK(BM_NAIVE_SDOT) -> Args({100000});
-BENCHMARK(BM_EXO_SDOT) -> Args({100000});
 
-BENCHMARK(BM_CBLAS_SDOT) -> Args({1000000});
-BENCHMARK(BM_NAIVE_SDOT) -> Args({1000000});
-BENCHMARK(BM_EXO_SDOT) -> Args({1000000});
+BENCHMARK(BM_CBLAS_SDOT) -> Args({1024});
+BENCHMARK(BM_EXO_SDOT) -> Args({1024});
 
-BENCHMARK(BM_CBLAS_SDOT) -> Args({10000000});
-BENCHMARK(BM_NAIVE_SDOT) -> Args({10000000});
-BENCHMARK(BM_EXO_SDOT) -> Args({10000000});
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({2048});
+BENCHMARK(BM_EXO_SDOT) -> Args({2048});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({4096});
+BENCHMARK(BM_EXO_SDOT) -> Args({4096});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({8192});
+BENCHMARK(BM_EXO_SDOT) -> Args({8192});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({16384});
+BENCHMARK(BM_EXO_SDOT) -> Args({16384});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({32768});
+BENCHMARK(BM_EXO_SDOT) -> Args({32768});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({65536});
+BENCHMARK(BM_EXO_SDOT) -> Args({65536});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({131072});
+BENCHMARK(BM_EXO_SDOT) -> Args({131072});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({262144});
+BENCHMARK(BM_EXO_SDOT) -> Args({262144});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({524288});
+BENCHMARK(BM_EXO_SDOT) -> Args({524288});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({1048576});
+BENCHMARK(BM_EXO_SDOT) -> Args({1048576});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({2097152});
+BENCHMARK(BM_EXO_SDOT) -> Args({2097152});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({4194304});
+BENCHMARK(BM_EXO_SDOT) -> Args({4194304});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({8388608});
+BENCHMARK(BM_EXO_SDOT) -> Args({8388608});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({16777216});
+BENCHMARK(BM_EXO_SDOT) -> Args({16777216});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({33554432});
+BENCHMARK(BM_EXO_SDOT) -> Args({33554432});
+
+
+BENCHMARK(BM_CBLAS_SDOT) -> Args({67108864});
+BENCHMARK(BM_EXO_SDOT) -> Args({67108864});
+
--- test/level1/dot/correctness.cpp
+++ test/level1/dot/correctness.cpp
@@ -1,4 +1,43 @@
+#include <vector>
+#include <math.h>
+#include <algorithm>
 
-int main() {
-    return 0;
+#include <cblas.h>
+
+#include "generate_buffer.h"
+
+#include "exo_dot.h"
+
+void test_sdot(int n, int incx, int incy) {
+    printf("Running sdot test: n = %d, incx = %d, incy = %d\n", n, incx, incy);
+    auto x = generate1d_sbuffer(n, incx);
+    auto y = generate1d_sbuffer(n, incy);
+    auto xcopy = x;
+    auto ycopy = y;
+
+    float result;
+    exo_sdot(nullptr, n, 
+    exo_win_1f32c{.data = x.data(), .strides = {incx}},
+    exo_win_1f32c{.data = y.data(), .strides = {incy}}, &result);
+    auto expected = cblas_sdot(n, xcopy.data(), incx, ycopy.data(), incy);
+
+    auto error = fabsf(result - expected);
+    auto rel_error = error / std::max(1.f, fabsf(expected));
+    auto epsilon = 1.f / 1000.f;
+
+    if (rel_error >= epsilon) {
+        printf("Failed! Expected %f, got %f\n", expected, result);
+        exit(1);
+    }
+
+    printf("Passed!\n");
+}
+
+int main () {
+    test_sdot(1, 1, 1);
+    test_sdot(2, 1, 1);
+    test_sdot(8, 1, 1);
+    test_sdot(100, 1, 1);
+    test_sdot(64 * 64 * 64, 1, 1);
+    test_sdot(100000000, 1, 1);
 }
