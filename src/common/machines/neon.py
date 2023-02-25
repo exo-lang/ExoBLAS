14e1e77d685caabf2c30301ec83c7dfc0dec25ec
Hooninator <jbellavita@berkeley.edu>
2023-02-25 10:13:14 +0000

    add instructions for l3 scaling to neon.py

--- src/common/machines/neon.py
+++ src/common/machines/neon.py
@@ -1,11 +1,30 @@
 from __future__ import annotations
 
 from exo.platforms.neon import *
-
+from exo import instr
 
 from .machine import MachineParameters
 
 
+@instr("{dst_data} = vmulq_f32({dst_data}, {rhs_data});")
+def neon_vmul_4xf32_alias_hack(
+    dst: [f32][4] @ Neon4f, rhs: [f32][4] @ Neon4f
+):
+    assert stride(dst, 0) == 1
+    assert stride(rhs, 0) == 1
+
+    for i in seq(0, 4):
+        dst[i] = dst[i] * rhs[i]
+
+@instr("{dst_data} = {src_data};")
+def neon_reg_copy_4xf32(
+    dst: [f32][4] @ Neon4f, src: [f32][4] @ Neon4f
+):
+    assert stride(dst, 0) == 1
+    assert stride(src, 0) == 1
+
+    for i in seq(0, 4):
+        dst[i] = src[i]
 
 Machine = MachineParameters(
     name="neon",
