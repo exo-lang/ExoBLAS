a290e842f2bacf84a92560dade10cab72c53c7b0
Yuka Ikarashi <yukatkh@gmail.com>
2023-02-28 22:25:40 -0500

    update graph.py

--- test/graph.py
+++ test/graph.py
@@ -1,7 +1,32 @@
 import json
 import sys
+import matplotlib.pyplot as plt
 
-if len(sys.argv) != 2:
-    print("error!")
+if len(sys.argv) != 3:
+    print("python grpah.py <kernel name> <google benchmark output json file>!")
 
-print(sys.argv[1])
+kernel_name = sys.argv[1]
+
+with open(sys.argv[2]) as f:
+    data = json.load(f)
+
+# TODO: We should probably use this context data for time tracking
+context = data['context']
+
+perf_res = {}
+for d in data['benchmarks']:
+    lis = d['name'].split('/')
+    name = lis[0]
+    size = lis[1]
+    time = d['cpu_time']
+    p = perf_res.setdefault( name, ([size], [time]) )
+    p[0].append(size)
+    p[1].append(time)
+
+for key in perf_res:
+    plt.plot(perf_res[key][0], perf_res[key][1], label=key)
+    plt.legend()
+
+plt.ylabel('runtime (ns)')
+plt.xlabel('size')
+plt.savefig(kernel_name+'.png')
