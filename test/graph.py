65de09783721b9c20ffc48dd8c9fc6c1a57f3865
Yuka Ikarashi <yukatkh@gmail.com>
2023-03-08 16:00:25 -0500

    graph script working for level1 but nothing else

--- test/graph.py
+++ test/graph.py
@@ -1,11 +1,15 @@
 import json
 import sys
 import matplotlib.pyplot as plt
+import math
+
+vectors = {"snrm2" : 1, "sscal": 1, "scopy": 2, "srot":2, "sswap": 2, "sasum": 1, "sdot":2}
 
 if len(sys.argv) != 3:
     print("python grpah.py <kernel name> <google benchmark output json file>!")
 
 kernel_name = sys.argv[1]
+plt.title(kernel_name + " kernel with AVX2")
 
 with open(sys.argv[2]) as f:
     data = json.load(f)
@@ -16,12 +20,18 @@ context = data['context']
 perf_res = {}
 for d in data['benchmarks']:
     lis = d['name'].split('/')
-    name = lis[0]
-    size = int(lis[1])
-    time = d['cpu_time']
-    p = perf_res.setdefault( name, ([size], [time]) )
+    name = lis[0].split('_')
+    name = " ".join(name[1:])
+    size = int(lis[1]) # Words
+    time = d['cpu_time'] # ns
+    # size : word already
+    # time is nanoseconds
+    gword_sec = (size*vectors.get(kernel_name, 2)*10**9)/(time*2**30) # (Words*1 / ns) => Gwords/s
+    size = math.log(size)
+    #print(size, time, gword_sec)
+    p = perf_res.setdefault( name, ([size], [gword_sec]) )
     p[0].append(size)
-    p[1].append(time)
+    p[1].append(gword_sec)
 
 for key in perf_res:
     sorted_points = sorted([(perf_res[key][0][i], perf_res[key][1][i]) for i in range(len(perf_res[key][0]))])
@@ -30,6 +40,12 @@ for key in perf_res:
     plt.plot(x, y, label=key)
     plt.legend()
 
-plt.ylabel('runtime (ns)')
-plt.xlabel('size')
+f = math.log
+peak_x = [0, f(32*1024/4), f(256*1024/4), f(6*1024*1024/4), f(66*1024*1024/4)]
+peak_y = [60.764375, 26.390875, 14.170275, 8.750275, 8.750275]
+plt.plot(peak_x, peak_y, drawstyle='steps-post', label="peak bandwidth")
+plt.legend()
+
+plt.ylabel('Gwords/sec')
+plt.xlabel('log(words)')
 plt.savefig(kernel_name+'.png')
