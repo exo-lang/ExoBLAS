a8e41dd4e4b3b84295920dc74a4cc52c1ae6cdbf
Samir Droubi <samirdr@mit.edu>
2023-04-14 00:09:46 -0400

    started work on scheduling trmv
    - simple vectorization for nonTrans version
    - rewrote trans version as operation on columns of A^T
    - split nonTrans version based on Diag

--- test/level2/strmv/exo_strmv.h
+++ test/level2/strmv/exo_strmv.h
@@ -16,16 +16,26 @@ void exo_strmv(const enum CBLAS_ORDER order, const enum CBLAS_UPLO Uplo,
   }
   if (Uplo == CBLAS_UPLO::CblasUpper) {
     if (TransA == CBLAS_TRANSPOSE::CblasNoTrans) {
-      if (incX == 1) {
-        exo_strmv_row_major_Upper_NonTrans_stride_1(
-            nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
-            exo_win_2f32c{.data = A, .strides = {lda, 1}},
-            Diag == CBLAS_DIAG::CblasUnit);
+      if (Diag == CBLAS_DIAG::CblasUnit) {
+        if (incX == 1) {
+          exo_strmv_row_major_Upper_NonTrans_Unit_stride_1(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        } else {
+          exo_strmv_row_major_Upper_NonTrans_Unit_stride_any(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        }
       } else {
-        exo_strmv_row_major_Upper_NonTrans_stride_any(
-            nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
-            exo_win_2f32c{.data = A, .strides = {lda, 1}},
-            Diag == CBLAS_DIAG::CblasUnit);
+        if (incX == 1) {
+          exo_strmv_row_major_Upper_NonTrans_NonUnit_stride_1(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        } else {
+          exo_strmv_row_major_Upper_NonTrans_NonUnit_stride_any(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        }
       }
     } else {
       exo_strmv_row_major_Upper_Trans_stride_any(
@@ -35,16 +45,26 @@ void exo_strmv(const enum CBLAS_ORDER order, const enum CBLAS_UPLO Uplo,
     }
   } else {
     if (TransA == CBLAS_TRANSPOSE::CblasNoTrans) {
-      if (incX == 1) {
-        exo_strmv_row_major_Lower_NonTrans_stride_1(
-            nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
-            exo_win_2f32c{.data = A, .strides = {lda, 1}},
-            Diag == CBLAS_DIAG::CblasUnit);
+      if (Diag == CBLAS_DIAG::CblasUnit) {
+        if (incX == 1) {
+          exo_strmv_row_major_Lower_NonTrans_Unit_stride_1(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        } else {
+          exo_strmv_row_major_Lower_NonTrans_Unit_stride_any(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        }
       } else {
-        exo_strmv_row_major_Lower_NonTrans_stride_any(
-            nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
-            exo_win_2f32c{.data = A, .strides = {lda, 1}},
-            Diag == CBLAS_DIAG::CblasUnit);
+        if (incX == 1) {
+          exo_strmv_row_major_Lower_NonTrans_NonUnit_stride_1(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        } else {
+          exo_strmv_row_major_Lower_NonTrans_NonUnit_stride_any(
+              nullptr, N, exo_win_1f32{.data = X, .strides = {incX}},
+              exo_win_2f32c{.data = A, .strides = {lda, 1}});
+        }
       }
     } else {
       exo_strmv_row_major_Lower_Trans_stride_any(
