56ec03f8bf15008afd652f798040fd28738959e4
Kevin Qian <keqian@mit.edu>
2023-04-19 13:27:43 -0400

    Merged

--- run_level2_tests.sh
+++ run_level2_tests.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-KERNELS=("syr" "syr2" "ger" "gemv" "gbmv" "trmv")
+#KERNELS=("syr" "syr2" "ger" "gemv" "gbmv" "trmv")
+KERNELS=("ger")
 
-for bla_vendor in "OpenBLAS" "Intel10_64lp_seq"
+#PRESET=avx2
+PRESET=apple-silicon
+
+#for bla_vendor in "OpenBLAS" "Intel10_64lp_seq"
+for bla_vendor in "OpenBLAS" "Apple"
 do
-  cmake --preset avx2 -DCMAKE_PREFIX_PATH=~/benchmark/build/ -DBLA_VENDOR=$bla_vendor
+  # cmake --preset $PRESET -DCMAKE_PREFIX_PATH=~/benchmark/build/ -DBLA_VENDOR=$bla_vendor
+  cmake --preset $PRESET -DCMAKE_PREFIX_PATH=~/benchmark/build/ -DBLA_VENDOR=$bla_vendor -DBLAS_openblas_LIBRARY=/opt/homebrew/opt/openblas/lib/libopenblas.dylib 
   for kernel in ${KERNELS[@]}; do
-    cmake --build build/avx2/ --target exo_${kernel}
+    cmake --build build/${PRESET}/ --target exo_${kernel}
     for precision in "s" "d"; do
-      cmake --build build/avx2/ --target ${precision}${kernel}_bench
-      ctest --test-dir build/avx2 -R cblas_${precision}${kernel}_bench
+      cmake --build build/${PRESET}/ --target ${precision}${kernel}_bench
+      ctest --test-dir build/${PRESET} -R cblas_${precision}${kernel}_bench
     done
   done
 done
 
 for kernel in ${KERNELS[@]}; do
   for precision in "s" "d"; do
-    ctest --test-dir build/avx2 -R exo_${precision}${kernel}_bench
-    ctest --test-dir build/avx2 -R ${precision}${kernel}_graph
+    ctest --test-dir build/${PRESET} -R exo_${precision}${kernel}_bench
+    ctest --test-dir build/${PRESET} -R ${precision}${kernel}_graph
   done
 done
