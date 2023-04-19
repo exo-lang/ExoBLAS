ba3435a9b45f6501a64aac6d930e6db706f6a64f
Kevin Qian <keqian@mit.edu>
2023-04-19 12:00:04 -0400

    added dgemv and script for running tests

---
+++ run_level2_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# KERNELS=("gemv" "gbmv" "trmv" "ger" "syr" "syr2")
+KERNELS=("gemv")
+
+for bla_vendor in "OpenBLAS" "Intel10_64lp_seq"
+do
+  cmake --preset avx2 -DCMAKE_PREFIX_PATH=~/benchmark/build/ -DBLA_VENDOR=$bla_vendor
+  for kernel in ${KERNELS}; do
+    cmake --build build/avx2/ --target exo_${kernel}
+    for precision in "s" "d"; do
+      cmake --build build/avx2/ --target ${precision}${kernel}_bench
+      ctest --test-dir build/avx2 -R cblas_${precision}${kernel}_bench
+    done
+  done
+done
+
+for kernel in ${KERNELS}; do
+  for precision in "s" "d"; do
+    ctest --test-dir build/avx2 -R exo_${precision}${kernel}_bench
+    ctest --test-dir build/avx2 -R ${precision}${kernel}_graph
+  done
+done
---
+++ test/level2/dgemv/bench.cpp
@@ -0,0 +1,117 @@
+#include <benchmark/benchmark.h>
+#include <cblas.h>
+
+#include <vector>
+
+#include "exo_dgemv.h"
+#include "generate_buffer.h"
+
+static void BM_cblas_dgemv(benchmark::State &state) {
+  const int N = state.range(0);
+  const int M = N;
+  const enum CBLAS_ORDER order = state.range(1) == 0
+                                     ? CBLAS_ORDER::CblasRowMajor
+                                     : CBLAS_ORDER::CblasColMajor;
+  const enum CBLAS_TRANSPOSE TransA = state.range(2) == 0
+                                          ? CBLAS_TRANSPOSE::CblasNoTrans
+                                          : CBLAS_TRANSPOSE::CblasTrans;
+  const double alpha = state.range(3);
+  const int lda = state.range(4) + N;
+  const int incX = state.range(5);
+  const double beta = state.range(6);
+  const int incY = state.range(7);
+  size_t alignmentA = state.range(8);
+  size_t alignmentX = state.range(9);
+  size_t alignmentY = state.range(10);
+
+  auto A = AlignedBuffer<double>(N * lda, 1, alignmentA);
+  int sizeX = N;
+  int sizeY = M;
+  if (TransA == CBLAS_TRANSPOSE::CblasTrans) {
+    sizeX = M;
+    sizeY = N;
+  }
+
+  auto X = AlignedBuffer<double>(sizeX, incX);
+  auto Y = AlignedBuffer<double>(sizeY, incY);
+
+  for (auto _ : state) {
+    cblas_dgemv(order, TransA, N, N, alpha, A.data(), lda, X.data(), incX, beta,
+                Y.data(), incY);
+  }
+}
+
+static void BM_exo_dgemv(benchmark::State &state) {
+  const int N = state.range(0);
+  const int M = N;
+  const enum CBLAS_ORDER order = state.range(1) == 0
+                                     ? CBLAS_ORDER::CblasRowMajor
+                                     : CBLAS_ORDER::CblasColMajor;
+  const enum CBLAS_TRANSPOSE TransA = state.range(2) == 0
+                                          ? CBLAS_TRANSPOSE::CblasNoTrans
+                                          : CBLAS_TRANSPOSE::CblasTrans;
+  const double alpha = state.range(3);
+  const int lda = state.range(4) + N;
+  const int incX = state.range(5);
+  const double beta = state.range(6);
+  const int incY = state.range(7);
+  size_t alignmentA = state.range(8);
+  size_t alignmentX = state.range(9);
+  size_t alignmentY = state.range(10);
+
+  auto A = AlignedBuffer<double>(M * lda, 1, alignmentA);
+  int sizeX = N;
+  int sizeY = M;
+  if (TransA == CBLAS_TRANSPOSE::CblasTrans) {
+    sizeX = M;
+    sizeY = N;
+  }
+
+  auto X = AlignedBuffer<double>(sizeX, incX);
+  auto Y = AlignedBuffer<double>(sizeY, incY);
+
+  for (auto _ : state) {
+    exo_dgemv(order, TransA, N, N, alpha, A.data(), lda, X.data(), incX, beta,
+              Y.data(), incY);
+  }
+}
+
+static void CustomArgumentsPacked(benchmark::internal::Benchmark *b) {
+  for (int order = 0; order < 1; ++order) {
+    for (int TransA = 0; TransA <= 1; ++TransA) {
+      for (int alpha = 3; alpha <= 3; ++alpha) {
+        for (int lda_diff = 0; lda_diff <= 0; ++lda_diff) {
+          for (int incX = 1; incX <= 1; ++incX) {
+            for (int beta = 5; beta <= 5; ++beta) {
+              for (int incY = 1; incY <= 1; ++incY) {
+                for (int alignmentA = 64; alignmentA <= 64; ++alignmentA) {
+                  for (int alignmentX = 64; alignmentX <= 64; ++alignmentX) {
+                    for (int alignmentY = 64; alignmentY <= 64; ++alignmentY) {
+                      for (int N = 1; N <= (1 << 13); N *= 2) {
+                        b->Args({N, order, TransA, alpha, lda_diff, incX, beta,
+                                 incY, alignmentA, alignmentX, alignmentY});
+                      }
+                      for (int N = 1; N <= (1 << 13); N *= 7) {
+                        b->Args({N, order, TransA, alpha, lda_diff, incX, beta,
+                                 incY, alignmentA, alignmentX, alignmentY});
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+BENCHMARK(BM_cblas_dgemv)
+    ->ArgNames({"N", "order", "TransA", "alpha", "lda_diff", "incX", "beta",
+                "incY", "alignmentA", "alignmentX", "alignmentY"})
+    ->Apply(CustomArgumentsPacked);
+BENCHMARK(BM_exo_dgemv)
+    ->ArgNames({"N", "order", "TransA", "alpha", "lda_diff", "incX", "beta",
+                "incY", "alignmentA", "alignmentX", "alignmentY"})
+    ->Apply(CustomArgumentsPacked);
