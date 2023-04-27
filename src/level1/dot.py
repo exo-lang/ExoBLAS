837a76ad00d6c7e4c9001adfe1aff03985737a21
Samir Droubi <samir.t.droubi@gmail.com>
2023-04-27 12:02:36 -0400

    Simplify dot schedule

--- src/level1/dot.py
+++ src/level1/dot.py
@@ -5,9 +5,21 @@ from exo.libs.memories import DRAM_STATIC
 from exo.platforms.x86 import *
 from exo.syntax import *
 from exo.stdlib.scheduling import *
+import exo.API_cursors as pc
 
 import exo_blas_config as C
-from composed_schedules import vectorize
+from composed_schedules import (
+    apply_to_block,
+    hoist_stmt,
+)
+from blas_composed_schedules import blas_vectorize
+from codegen_helpers import (
+    specialize_precision,
+    generate_stride_any_proc,
+    export_exo_proc,
+    generate_stride_1_proc,
+)
+from parameters import Level_1_Params
 
 
 ### EXO_LOC ALGORITHM START ###
@@ -22,112 +34,21 @@ def dot_template(n: size, x: [R][n], y: [R][n], result: R):
 
 
 ### EXO_LOC SCHEDULE START ###
-def specialize_precision(precision):
-    prefix = "s" if precision == "f32" else "d"
-    specialized_copy = rename(dot_template, "exo_" + prefix + "dot")
-    for arg in ["x", "y", "result"]:
-        specialized_copy = set_precision(specialized_copy, arg, precision)
-    return specialized_copy
+def schedule_dot_stride_1(dot, params):
+    dot = generate_stride_1_proc(dot, precision)
+    main_loop = dot.find_loop("i")
+    dot = blas_vectorize(dot, main_loop, params)
+    return simplify(dot)
 
 
-def schedule_dot_stride_1_interleaved(
-    VEC_W, INTERLEAVE_FACTOR, memory, instructions, precision
-):
-    simple_stride_1 = specialize_precision(precision)
-    simple_stride_1 = rename(simple_stride_1, simple_stride_1.name() + "_stride_1")
-    simple_stride_1 = simple_stride_1.add_assertion("stride(x, 0) == 1")
-    simple_stride_1 = simple_stride_1.add_assertion("stride(y, 0) == 1")
-
-    loop_cursor = simple_stride_1.find_loop("i")
-    simple_stride_1 = vectorize(
-        simple_stride_1,
-        loop_cursor,
-        VEC_W,
-        INTERLEAVE_FACTOR,
-        INTERLEAVE_FACTOR,
-        memory,
-        precision,
-        instructions,
-    )
-    return simplify(simple_stride_1)
-
-
-INTERLEAVE_FACTOR = C.Machine.vec_units * 2
-
-#################################################
-# Generate specialized kernels for f32 precision
-#################################################
-
-exo_sdot_stride_any = specialize_precision("f32")
-exo_sdot_stride_any = rename(
-    exo_sdot_stride_any, exo_sdot_stride_any.name() + "_stride_any"
-)
-
-f32_instructions = [
-    C.Machine.load_instr_f32,
-    C.Machine.store_instr_f32,
-    C.Machine.assoc_reduce_add_instr_f32,
-    C.Machine.set_zero_instr_f32,
-    C.Machine.fmadd_instr_f32,
-    C.Machine.reg_copy_instr_f32,
+template_sched_list = [
+    (dot_template, schedule_dot_stride_1),
 ]
 
-if None not in f32_instructions:
-    exo_sdot_stride_1 = schedule_dot_stride_1_interleaved(
-        C.Machine.vec_width,
-        INTERLEAVE_FACTOR,
-        C.Machine.mem_type,
-        f32_instructions,
-        "f32",
-    )
-else:
-    exo_sdot_stride_1 = specialize_precision("f32")
-    exo_sdot_stride_1 = rename(
-        exo_sdot_stride_1, exo_sdot_stride_1.name() + "_stride_1"
-    )
-
-#################################################
-# Generate specialized kernels for f64 precision
-#################################################
-
-exo_ddot_stride_any = specialize_precision("f64")
-exo_ddot_stride_any = rename(
-    exo_ddot_stride_any, exo_ddot_stride_any.name() + "_stride_any"
-)
-
-f64_instructions = [
-    C.Machine.load_instr_f64,
-    C.Machine.store_instr_f64,
-    C.Machine.assoc_reduce_add_instr_f64,
-    C.Machine.set_zero_instr_f64,
-    C.Machine.fmadd_instr_f64,
-    C.Machine.reg_copy_instr_f64,
-]
-
-if None not in f64_instructions:
-    exo_ddot_stride_1 = schedule_dot_stride_1_interleaved(
-        C.Machine.vec_width // 2,
-        INTERLEAVE_FACTOR,
-        C.Machine.mem_type,
-        f64_instructions,
-        "f64",
-    )
-else:
-    exo_ddot_stride_1 = specialize_precision("f64")
-    exo_ddot_stride_1 = rename(
-        exo_ddot_stride_1, exo_ddot_stride_1.name() + "_stride_1"
-    )
+for precision in ("f32", "f64"):
+    for template, sched in template_sched_list:
+        proc_stride_any = generate_stride_any_proc(template, precision)
+        export_exo_proc(globals(), proc_stride_any)
+        proc_stride_1 = sched(template, Level_1_Params(precision=precision))
+        export_exo_proc(globals(), proc_stride_1)
 ### EXO_LOC SCHEDULE END ###
-
-entry_points = [
-    exo_sdot_stride_any,
-    exo_sdot_stride_1,
-    exo_ddot_stride_any,
-    exo_ddot_stride_1,
-]
-
-if __name__ == "__main__":
-    for p in entry_points:
-        print(p)
-
-__all__ = [p.name() for p in entry_points]
