d46325bbfa25aede937bf0732b1efbfa9fb0d46e
Samir Droubi <samirdr@mit.edu>
2023-04-30 14:26:54 -0400

    Fail-fast check in hoist

--- src/common/introspection.py
+++ src/common/introspection.py
@@ -20,3 +20,41 @@ def get_statemnts(proc):
                 yield from get_statemnts_helper(stmt.body())
 
     return get_statemnts_helper(proc.body())
+
+
+def get_expr_dependencies(expr):
+    if isinstance(expr, ExprListCursor):
+        for e in expr:
+            yield from get_expr_dependencies(e)
+    elif isinstance(expr, ReadCursor):
+        yield expr.name()
+    elif isinstance(expr, UnaryMinusCursor):
+        yield from get_expr_dependencies(expr.arg())
+    elif isinstance(expr, BuiltInFunctionCursor):
+        yield from get_expr_dependencies(expr.args())
+    elif isinstance(expr, BinaryOpCursor):
+        yield from get_expr_dependencies(expr.lhs(), expr.rhs())
+    elif isinstance(expr, WindowExprCursor):
+        yield from get_expr_dependencies(expr.idx())
+
+
+def get_stmt_dependencies(stmt):
+    if isinstance(stmt, BlockCursor):
+        for s in stmt:
+            yield from get_stmt_dependencies(s)
+    elif isinstance(stmt, (ReadCursor, ReduceCursor)):
+        yield stmt.name()
+        yield from get_expr_dependencies(stmt.idx())
+        yield from get_expr_dependencies(stmt.rhs())
+    elif isinstance(stmt, CallCursor):
+        yield from get_expr_dependencies(stmt.args())
+    elif isinstance(stmt, IfCursor):
+        yield from get_expr_dependencies(stmt.cond())
+        yield from get_stmt_dependencies(stmt.body())
+    elif isinstance(stmt, ForSeqCursor):
+        yield from get_expr_dependencies(stmt.hi())
+        yield from get_stmt_dependencies(stmt.body())
+    elif isinstance(stmt, AllocCursor):
+        yield from get_expr_dependencies(stmt.shape())
+    elif isinstance(stmt, WindowStmtCursor):
+        yield from get_expr_dependencies(stmt.winexpr())
