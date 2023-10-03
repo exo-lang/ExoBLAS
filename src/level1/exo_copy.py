1d87e6da3469032b82751d565048721e4160fb13
Samir Droubi <samir.t.droubi@gmail.com>
2023-10-03 02:20:36 -0400

    Rewrote copy

--- src/level1/exo_copy.py
+++ src/level1/exo_copy.py
@@ -6,8 +6,13 @@ from exo.platforms.x86 import *
 from exo.syntax import *
 from exo.stdlib.scheduling import *
 
-import exo_blas_config as C
-from composed_schedules import vectorize_to_loops, interleave_execution
+from blas_composed_schedules import blas_vectorize
+from codegen_helpers import (
+    generate_stride_any_proc,
+    export_exo_proc,
+    generate_stride_1_proc,
+)
+from parameters import Level_1_Params
 
 
 ### EXO_LOC ALGORITHM START ###
@@ -20,74 +25,27 @@ def copy_template(n: size, x: [R][n], y: [R][n]):
 ### EXO_LOC ALGORITHM END ###
 
 ### EXO_LOC SCHEDULE START ###
-def specialize_precision(precision):
-    prefix = "s" if precision == "f32" else "d"
-    specialized_copy = rename(copy_template, "exo_" + prefix + "copy")
-    for arg in ["x", "y"]:
-        specialized_copy = set_precision(specialized_copy, arg, precision)
-    return specialized_copy
+def schedule_copy(exo_copy, params):
+    exo_copy = generate_stride_1_proc(exo_copy, params.precision)
+    main_loop = exo_copy.find_loop("i")
+    exo_copy = blas_vectorize(exo_copy, main_loop, params)
+    return simplify(exo_copy)
 
 
-def schedule_scopy_stride_1(VEC_W, INTERLEAVE_FACTOR, memory, instructions, precision):
-    simple_stride_1 = specialize_precision(precision)
-    simple_stride_1 = rename(simple_stride_1, simple_stride_1.name() + "_stride_1")
-    simple_stride_1 = simple_stride_1.add_assertion("stride(x, 0) == 1")
-    simple_stride_1 = simple_stride_1.add_assertion("stride(y, 0) == 1")
-
-    simple_stride_1 = vectorize_to_loops(
-        simple_stride_1, simple_stride_1.find_loop("i"), VEC_W, memory, precision
-    )
-    simple_stride_1 = interleave_execution(
-        simple_stride_1, simple_stride_1.find_loop("io"), INTERLEAVE_FACTOR
-    )
-    simple_stride_1 = replace_all(simple_stride_1, instructions)
-
-    return simplify(simple_stride_1)
-
-
-INTERLEAVE_FACTOR = C.Machine.vec_units * 2
-
-f32_instructions = [C.Machine.load_instr_f32, C.Machine.store_instr_f32]
-
-exo_scopy_stride_1 = schedule_scopy_stride_1(
-    C.Machine.vec_width, INTERLEAVE_FACTOR, C.Machine.mem_type, f32_instructions, "f32"
-)
-exo_scopy_stride_any = specialize_precision("f32")
-exo_scopy_stride_any = rename(
-    exo_scopy_stride_any, exo_scopy_stride_any.name() + "_stride_any"
-)
-
-f64_instructions = [C.Machine.load_instr_f64, C.Machine.store_instr_f64]
-
-if None not in f64_instructions:
-    exo_dcopy_stride_1 = schedule_scopy_stride_1(
-        C.Machine.vec_width // 2,
-        INTERLEAVE_FACTOR,
-        C.Machine.mem_type,
-        f64_instructions,
-        "f64",
-    )
-else:
-    exo_dcopy_stride_1 = specialize_precision("f64")
-    exo_dcopy_stride_1 = rename(
-        exo_dcopy_stride_1, exo_dcopy_stride_1.name() + "_stride_1"
-    )
-
-exo_dcopy_stride_any = specialize_precision("f64")
-exo_dcopy_stride_any = rename(
-    exo_dcopy_stride_any, exo_dcopy_stride_any.name() + "_stride_any"
-)
-### EXO_LOC SCHEDULE END ###
-
-entry_points = [
-    exo_scopy_stride_1,
-    exo_dcopy_stride_1,
-    exo_scopy_stride_any,
-    exo_dcopy_stride_any,
+template_sched_list = [
+    (copy_template, schedule_copy),
 ]
 
-if __name__ == "__main__":
-    for p in entry_points:
-        print(p)
+for precision in ("f32", "f64"):
+    for template, sched in template_sched_list:
+        proc_stride_any = generate_stride_any_proc(template, precision)
+        export_exo_proc(globals(), proc_stride_any)
+        proc_stride_1 = sched(
+            template,
+            Level_1_Params(
+                precision=precision, accumulators_count=1, interleave_factor=4
+            ),
+        )
+        export_exo_proc(globals(), proc_stride_1)
 
-__all__ = [p.name() for p in entry_points]
+### EXO_LOC SCHEDULE END ###
