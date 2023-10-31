2c902cc96f3efb8239784c6e9a70379cdef168c1
Yuka Ikarashi <yukatkh@gmail.com>
2023-10-31 16:44:00 -0400

    Implement clean Gemmini Gemm and preliminary SAD (#30)
    SAD is WIP, need to define i8 AVX instructions, support i64 in Exo, and
    write functional and benchmark tests. Anyways this code should
    eventually moved to some other place not inside BLAS.

---
+++ src/level1/sad.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+from exo import *
+from exo.libs.memories import DRAM_STATIC
+from exo.platforms.x86 import *
+from exo.syntax import *
+from exo.stdlib.scheduling import *
+
+import exo_blas_config as C
+from composed_schedules import (
+    auto_divide_loop,
+    vectorize_to_loops,
+    vectorize,
+    interleave_execution,
+    parallelize_reduction,
+    stage_expr,
+    auto_stage_mem,
+)
+from codegen_helpers import (
+    generate_stride_any_proc,
+    export_exo_proc,
+    generate_stride_1_proc,
+    bind_builtins_args,
+)
+from parameters import Level_1_Params
+
+
+@instr("{dst_data} = _mm256_sad_epu8(&{src1_data}, &{src2_data});")
+def mm256_sad_epu8(dst: [R][4] @ AVX2, src1: [i8][32] @ AVX2, src2: [i8][32] @ AVX2):
+    for i in seq(0, 4):
+        tmp: R @ AVX2
+        tmp = 0.0
+        for j in seq(0, 8):
+            tmp += select(
+                src1[i * 8 + j],
+                src2[i * 8 + j],
+                src1[i * 8 + j] - src2[i * 8 + j],
+                src2[i * 8 + j] - src1[i * 8 + j],
+            )
+        dst[i] = tmp
+
+
+@proc
+def sad(n: size, x: i8[n] @ DRAM, y: i8[n] @ DRAM, result: R @ DRAM):
+    result = 0.0
+    for i in seq(0, n):
+        result += select(y[i], x[i], x[i] - y[i], y[i] - x[i])
+
+
+sad = sad.partial_eval(1024)
+i_loop = sad.find_loop("i")
+
+# blocking, binding reg
+sad, loop_c = auto_divide_loop(sad, i_loop, 8, perfect=True)
+sad, _ = parallelize_reduction(sad, i_loop, "result", 4, AVX2, "R")
+sad = stage_mem(sad, loop_c.inner_loop_cursor, "reg[ioi]", "tmp_reg", accum=True)
+# sad = auto_stage_mem(sad, 'reg[ioi]', 'tmp_reg', accum=True)
+sad = simplify(sad)
+sad = stage_expr(sad, sad.find("tmp_reg"), "tmp_reg1", memory=AVX2)
+
+# binding x and y
+sad = auto_stage_mem(sad, sad.find("x[_]"), "xReg", n_lifts=2)
+sad = auto_stage_mem(sad, sad.find("y[_]"), "yReg", n_lifts=2)
+sad = simplify(sad)
+sad = set_memory(sad, "xReg:_", AVX2)
+sad = set_memory(sad, "yReg:_", AVX2)
+
+# FIXME: for now set all memory to DRAM so that the codegen doesn't complain
+sad = set_memory(sad, "xReg:_", DRAM)
+sad = set_memory(sad, "yReg:_", DRAM)
+sad = set_memory(sad, "reg:_", DRAM)
+sad = set_memory(sad, "tmp_reg1:_", DRAM)
+sad = set_memory(sad, "tmp_reg:_", DRAM)
+
+# replace
+# sad = replace_all(sad, [mm256_sad_epu8])
+sad = bind_builtins_args(sad, sad.body(), "i8")
+
+print(sad)
