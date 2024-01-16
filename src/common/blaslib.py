c9dec5764375bda089c36b9fe3139a56a04178ec
Samir Droubi <samirdr@mit.edu>
2024-01-16 18:00:31 -0500

    Simplify vectorize and support various tail strats

--- src/common/blaslib.py
+++ src/common/blaslib.py
@@ -23,51 +23,25 @@ def optimize_level_1(proc, loop, params):
 
     loop = proc.forward(loop)
 
-    # Determine the tail strategy
+    # Vectorization
     vectorize_tail = mem_type in {AVX2}
-    tail = "guard" if vectorize_tail else "cut"
-
-    # Tile to exploit vectorization
-    proc, (outer_loop, inner_loop, _) = auto_divide_loop(
-        proc, loop, vec_width, tail=tail
-    )
-
-    proc = parallelize_all_reductions(proc, inner_loop, mem_type)
-
-    # Previous step calls fission which would change what
-    # inner loop we are pointing at
-    outer_loop = proc.forward(outer_loop)
-    inner_loop = outer_loop.body()[0]
-
-    # Generate simd
-    proc = scalar_to_simd(proc, inner_loop, vec_width, mem_type, precision)
-
-    # Generate the vectorized tail
-    if vectorize_tail:
-        # Cut the tail iterations
-        last_outer_iteration = FormattedExprStr("_ - 1", outer_loop.hi())
-        proc = cut_loop(proc, outer_loop, last_outer_iteration)
-
-        # Eliminate the predication in the main inner loop
-        proc = dce(proc, outer_loop)
+    tail = "cut_and_predicate" if vectorize_tail else "cut"
+    proc = vectorize(proc, loop, vec_width, precision, mem_type, tail=tail)
 
     # Hoist any stmt
-    index_from_end = get_index_in_body(proc, outer_loop, False)
-    proc = hoist_from_loop(proc, outer_loop)
-    outer_loop = get_parent(proc, outer_loop).body()[index_from_end]
-
-    if interleave_factor == 1:
-        return simplify(replace_all(proc, instructions))
+    index_from_end = get_index_in_body(proc, loop, False)
+    proc = hoist_from_loop(proc, loop)
+    loop = get_parent(proc, loop).body()[index_from_end]
 
     # Tile to exploit ILP
-    proc, (outer_loop, inner_loop, _) = auto_divide_loop(
-        proc, outer_loop, interleave_factor, tail="cut"
+    proc, (loop, inner_loop, _) = auto_divide_loop(
+        proc, loop, interleave_factor, tail="cut"
     )
 
     proc = parallelize_all_reductions(proc, inner_loop, mem_type, unroll=True)
 
     # Intereleave to increase ILP
-    inner_loop = proc.forward(outer_loop).body()[0]
+    inner_loop = proc.forward(loop).body()[0]
     proc = interleave_execution(proc, inner_loop, interleave_factor)
 
     # Instructions Selection
