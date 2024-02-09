6502c81b14eabad2ae9df5a845ff57740741f2fc
Samir Droubi <samirdr@mit.edu>
2024-02-08 22:33:49 -0500

    Rewrite ger

--- test/level2/dger/exo_dger.h
+++ test/level2/dger/exo_dger.h
@@ -9,10 +9,10 @@ void exo_dger(const int M, const int N, const double alpha, const double *X,
     return;
   }
   if (incX == 1 && incY == 1) {
-    exo_dger_row_major_stride_1(nullptr, M, N, &alpha,
-                                exo_win_1f64c{.data = X, .strides = {incX}},
-                                exo_win_1f64c{.data = Y, .strides = {incY}},
-                                exo_win_2f64{.data = A, .strides = {lda, 1}});
+    exo_dger_rm_stride_1(nullptr, M, N, &alpha,
+                         exo_win_1f64c{.data = X, .strides = {incX}},
+                         exo_win_1f64c{.data = Y, .strides = {incY}},
+                         exo_win_2f64{.data = A, .strides = {lda, 1}});
   } else {
     if (incX < 0) {
       X = X + (1 - M) * incX;
@@ -20,9 +20,9 @@ void exo_dger(const int M, const int N, const double alpha, const double *X,
     if (incY < 0) {
       Y = Y + (1 - N) * incY;
     }
-    exo_dger_row_major_stride_any(nullptr, M, N, &alpha,
-                                  exo_win_1f64c{.data = X, .strides = {incX}},
-                                  exo_win_1f64c{.data = Y, .strides = {incY}},
-                                  exo_win_2f64{.data = A, .strides = {lda, 1}});
+    exo_dger_rm_stride_any(nullptr, M, N, &alpha,
+                           exo_win_1f64c{.data = X, .strides = {incX}},
+                           exo_win_1f64c{.data = Y, .strides = {incY}},
+                           exo_win_2f64{.data = A, .strides = {lda, 1}});
   }
 }
--- test/level2/sger/exo_sger.h
+++ test/level2/sger/exo_sger.h
@@ -9,10 +9,10 @@ void exo_sger(const int M, const int N, const float alpha, const float *X,
     return;
   }
   if (incX == 1 && incY == 1) {
-    exo_sger_row_major_stride_1(nullptr, M, N, &alpha,
-                                exo_win_1f32c{.data = X, .strides = {incX}},
-                                exo_win_1f32c{.data = Y, .strides = {incY}},
-                                exo_win_2f32{.data = A, .strides = {lda, 1}});
+    exo_sger_rm_stride_1(nullptr, M, N, &alpha,
+                         exo_win_1f32c{.data = X, .strides = {incX}},
+                         exo_win_1f32c{.data = Y, .strides = {incY}},
+                         exo_win_2f32{.data = A, .strides = {lda, 1}});
   } else {
     if (incX < 0) {
       X = X + (1 - M) * incX;
@@ -20,9 +20,9 @@ void exo_sger(const int M, const int N, const float alpha, const float *X,
     if (incY < 0) {
       Y = Y + (1 - N) * incY;
     }
-    exo_sger_row_major_stride_any(nullptr, M, N, &alpha,
-                                  exo_win_1f32c{.data = X, .strides = {incX}},
-                                  exo_win_1f32c{.data = Y, .strides = {incY}},
-                                  exo_win_2f32{.data = A, .strides = {lda, 1}});
+    exo_sger_rm_stride_any(nullptr, M, N, &alpha,
+                           exo_win_1f32c{.data = X, .strides = {incX}},
+                           exo_win_1f32c{.data = Y, .strides = {incY}},
+                           exo_win_2f32{.data = A, .strides = {lda, 1}});
   }
 }
