c96e0e1bfa7edcb29f93bbd0f539b140318b4dc2
Samir Droubi <samirdr@mit.edu>
2024-02-17 23:21:48 -0500

    Update trsv codegen

--- test/codegen/hash.py
+++ test/codegen/hash.py
@@ -17,7 +17,7 @@ VERBOSE = False
 
 
 # Be very catious adding tests here. Ideally, we don't want any.
-NON_DETERMINISTIC_TESTS = {"exo_trmv"}
+NON_DETERMINISTIC_TESTS = {"exo_trmv", "exo_syr"}
 
 
 def get_diff(file1, file2):
