7775a3b63e3c5eb35782cec9320c91d69081cf23
Samir Droubi <samirdr@mit.edu>
2024-04-08 21:31:39 -0400

    Handle tails from rounding down

--- src/level2/trsv.py
+++ src/level2/trsv.py
@@ -16,12 +16,10 @@ def trsv_rm_un(Diag: index, n: size, x: [R][n], A: [R][n, n]):
         dot = 0.0
         for j in seq(0, i):
             dot += A[n - i - 1, n - j - 1] * x[n - j - 1]
-        pivot: R
+
+        x[n - i - 1] = x[n - i - 1] - dot
         if Diag == 0:
-            pivot = A[n - i - 1, n - i - 1]
-        else:
-            pivot = 1.0
-        x[n - i - 1] = (x[n - i - 1] - dot) / pivot
+            x[n - i - 1] = x[n - i - 1] / A[n - i - 1, n - i - 1]
 
 
 @proc
@@ -34,13 +32,9 @@ def trsv_rm_ln(Diag: index, n: size, x: [R][n], A: [R][n, n]):
         for j in seq(0, i):
             dot += A[i, j] * x[j]
 
-        pivot: R
+        x[i] = x[i] - dot
         if Diag == 0:
-            pivot = A[i, i]
-        else:
-            pivot = 1.0
-
-        x[i] = (x[i] - dot) / pivot
+            x[i] = x[i] / A[i, i]
 
 
 @proc
@@ -50,12 +44,9 @@ def trsv_rm_ut(Diag: index, n: size, x: [R][n], A: [R][n, n]):
     for i in seq(0, n):
         dot[i] = 0.0
     for i in seq(0, n):
-        pivot: R
+        x[i] = x[i] - dot[i]
         if Diag == 0:
-            pivot = A[i, i]
-        else:
-            pivot = 1.0
-        x[i] = (x[i] - dot[i]) / pivot
+            x[i] = x[i] / A[i, i]
         for j in seq(0, n - i - 1):
             dot[n - j - 1] += A[i, n - j - 1] * x[i]
 
@@ -67,12 +58,9 @@ def trsv_rm_lt(Diag: index, n: size, x: [R][n], A: [R][n, n]):
     for i in seq(0, n):
         dot[i] = 0.0
     for i in seq(0, n):
-        pivot: R
+        x[n - i - 1] = x[n - i - 1] - dot[n - i - 1]
         if Diag == 0:
-            pivot = A[n - i - 1, n - i - 1]
-        else:
-            pivot = 1.0
-        x[n - i - 1] = (x[n - i - 1] - dot[n - i - 1]) / pivot
+            x[n - i - 1] = x[n - i - 1] / A[n - i - 1, n - i - 1]
         for j in seq(0, n - i - 1):
             dot[j] += A[n - i - 1, j] * x[n - i - 1]
 
@@ -99,7 +87,7 @@ def schedule_t(proc, i_loop, precision, machine, rows_factor, cols_factor):
 
 
 for trsv in trsv_rm_ut, trsv_rm_lt:
-    variants_generator(schedule_t)(trsv, "i #1", 4, 2, globals=globals())
+    variants_generator(identity_schedule)(trsv, "i #1", 4, 2, globals=globals())
 
 for trsv in trsv_rm_un, trsv_rm_ln:
-    variants_generator(optimize_level_2)(trsv, "i", 4, 2, round_up=False, globals=globals())
+    variants_generator(identity_schedule)(trsv, "i", 4, 2, round_up=False, globals=globals())
