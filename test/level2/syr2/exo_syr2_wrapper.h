85ecca07e26913a8ca779c2b68e5da39aa3cfef7
Samir Droubi <samirdr@mit.edu>
2024-04-25 11:21:11 -0400

    Fixup syr2

--- test/level2/syr2/exo_syr2_wrapper.h
+++ test/level2/syr2/exo_syr2_wrapper.h
@@ -16,42 +16,22 @@
     if (incY < 0) {                                                          \
       Y = Y + (1 - N) * incY;                                                \
     }                                                                        \
-    if (Uplo == CBLAS_UPLO::CblasUpper) {                                    \
-      if (incX == 1 && incY == 1) {                                          \
-        exo_##prefix##syr2_rm_u_stride_1(                                    \
-            nullptr, N, &alpha,                                              \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_2##exo_type{.data = A, .strides = {lda, 1}});            \
-      } else {                                                               \
-        exo_##prefix##syr2_rm_u_stride_any(                                  \
-            nullptr, N, &alpha,                                              \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_2##exo_type{.data = A, .strides = {lda, 1}});            \
-      }                                                                      \
+    if (incX == 1 && incY == 1) {                                            \
+      exo_##prefix##syr2_rm_stride_1(                                        \
+          nullptr, Uplo, N, &alpha,                                          \
+          exo_win_1##exo_type##c{.data = X, .strides = {incX}},              \
+          exo_win_1##exo_type##c{.data = X, .strides = {incX}},              \
+          exo_win_1##exo_type##c{.data = Y, .strides = {incY}},              \
+          exo_win_1##exo_type##c{.data = Y, .strides = {incY}},              \
+          exo_win_2##exo_type{.data = A, .strides = {lda, 1}});              \
     } else {                                                                 \
-      if (incX == 1 && incY == 1) {                                          \
-        exo_##prefix##syr2_rm_l_stride_1(                                    \
-            nullptr, N, &alpha,                                              \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_2##exo_type{.data = A, .strides = {lda, 1}});            \
-      } else {                                                               \
-        exo_##prefix##syr2_rm_l_stride_any(                                  \
-            nullptr, N, &alpha,                                              \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = X, .strides = {incX}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_1##exo_type##c{.data = Y, .strides = {incY}},            \
-            exo_win_2##exo_type{.data = A, .strides = {lda, 1}});            \
-      }                                                                      \
+      exo_##prefix##syr2_rm_stride_any(                                      \
+          nullptr, Uplo, N, &alpha,                                          \
+          exo_win_1##exo_type##c{.data = X, .strides = {incX}},              \
+          exo_win_1##exo_type##c{.data = X, .strides = {incX}},              \
+          exo_win_1##exo_type##c{.data = Y, .strides = {incY}},              \
+          exo_win_1##exo_type##c{.data = Y, .strides = {incY}},              \
+          exo_win_2##exo_type{.data = A, .strides = {lda, 1}});              \
     }                                                                        \
   }
 
