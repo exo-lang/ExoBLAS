a044474a5ca2621e16a25e6e12afe55a88e7a21f
Samir Droubi <samirdr@mit.edu>
2024-04-25 11:51:51 -0400

    Fixup trmv

--- src/level2/trmv.py
+++ src/level2/trmv.py
@@ -7,85 +7,52 @@ from codegen_helpers import *
 
 
 @proc
-def trmv_rm_un(Diag: index, n: size, x: [R][n], A: [R][n, n]):
+def trmv_rm(Uplo: size, TransA: size, Diag: size, n: size, x: [R][n], A: [R][n, n]):
     assert stride(A, 1) == 1
 
     xCopy: R[n]
 
-    for i in seq(0, n):
-        xCopy[n - i - 1] = 0.0
-        for j in seq(0, i):
-            xCopy[n - i - 1] += A[n - i - 1, n - j - 1] * x[n - j - 1]
-        if Diag == 0:
-            xCopy[n - i - 1] += A[n - i - 1, n - i - 1] * x[n - i - 1]
-        else:
-            xCopy[n - i - 1] += x[n - i - 1]
-
-    for l in seq(0, n):
-        x[l] = xCopy[l]
-
-
-@proc
-def trmv_rm_ln(Diag: index, n: size, x: [R][n], A: [R][n, n]):
-    assert stride(A, 1) == 1
-
-    xCopy: R[n]
-
-    for i in seq(0, n):
-        xCopy[i] = 0.0
-        for j in seq(0, i):
-            xCopy[i] += A[i, j] * x[j]
-        if Diag == 0:
-            xCopy[i] += A[i, i] * x[i]
-        else:
-            xCopy[i] += x[i]
-
-    for l in seq(0, n):
-        x[l] = xCopy[l]
-
-
-@proc
-def trmv_rm_ut(Diag: index, n: size, x: [R][n], A: [R][n, n]):
-    assert stride(A, 1) == 1
-
-    xCopy: R[n]
     for l in seq(0, n):
         xCopy[l] = 0.0
 
     for i in seq(0, n):
-        for j in seq(0, i):
-            xCopy[n - j - 1] += A[n - i - 1, n - j - 1] * x[n - i - 1]
-        if Diag == 0:
-            xCopy[n - i - 1] += A[n - i - 1, n - i - 1] * x[n - i - 1]
+        if TransA == CblasNoTransValue:
+            if Uplo == CblasUpperValue:
+                for j in seq(0, i):
+                    xCopy[n - i - 1] += A[n - i - 1, n - j - 1] * x[n - j - 1]
+                if Diag == CblasNonUnitValue:
+                    xCopy[n - i - 1] += A[n - i - 1, n - i - 1] * x[n - i - 1]
+                else:
+                    xCopy[n - i - 1] += x[n - i - 1]
+            else:
+                for j in seq(0, i):
+                    xCopy[i] += A[i, j] * x[j]
+                if Diag == CblasNonUnitValue:
+                    xCopy[i] += A[i, i] * x[i]
+                else:
+                    xCopy[i] += x[i]
         else:
-            xCopy[n - i - 1] += x[n - i - 1]
+            if Uplo == CblasUpperValue:
+                for j in seq(0, i):
+                    xCopy[n - j - 1] += A[n - i - 1, n - j - 1] * x[n - i - 1]
+                if Diag == CblasNonUnitValue:
+                    xCopy[n - i - 1] += A[n - i - 1, n - i - 1] * x[n - i - 1]
+                else:
+                    xCopy[n - i - 1] += x[n - i - 1]
+            else:
+                for j in seq(0, i):
+                    xCopy[j] += A[i, j] * x[i]
+                if Diag == CblasNonUnitValue:
+                    xCopy[i] += A[i, i] * x[i]
+                else:
+                    xCopy[i] += x[i]
 
     for l in seq(0, n):
         x[l] = xCopy[l]
 
 
-@proc
-def trmv_rm_lt(Diag: index, n: size, x: [R][n], A: [R][n, n]):
-    assert stride(A, 1) == 1
-
-    xCopy: R[n]
-    for l in seq(0, n):
-        xCopy[l] = 0.0
-
-    for i in seq(0, n):
-        for j in seq(0, i):
-            xCopy[j] += A[i, j] * x[i]
-        if Diag == 0:
-            xCopy[i] += A[i, i] * x[i]
-        else:
-            xCopy[i] += x[i]
-
-    for l in seq(0, n):
-        x[l] = xCopy[l]
+def schedule(trmv, loop, precision, machine, Uplo=None, TransA=None):
+    return optimize_level_2(trmv, loop, precision, machine, 4, 2, round_up=Uplo == CblasLowerValue)
 
 
-for proc in trmv_rm_ln, trmv_rm_lt:
-    variants_generator(optimize_level_2)(proc, "i", 4, 2, globals=globals())
-
-for proc in trmv_rm_un, trmv_rm_ut:
-    variants_generator(optimize_level_2)(proc, "i", 4, 2, round_up=False, globals=globals())
+variants_generator(schedule)(trmv_rm, "i", globals=globals())
